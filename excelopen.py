dbb191aeb284ead9221ad66ea2c7963ebdfaa76d
Fredrick W Warren <fredwarren@gmail.com>
2019-07-03 14:10:12 -0700

    addded methods to excelopen
    - open  to open an already existing sheet
    - from_template  to open from an exsiting sheet but save to another sheet
    - save  to save back to the opened sheet
    - saveas  to save to another sheet
    - close  to close current sheet

--- excelopen.py
+++ excelopen.py
@@ -23,16 +23,28 @@ class ExcelOpenDocument(object):
         self.workbook = Workbook()
         self.sheet = self.workbook.active
 
-    def open(self):
-        pass
+    def open(self, filename=None):
+        assert (filename), "No filename provided to open spreadsheet"
+        self.filename = filename
+        self.workbook = load_workbook(filename)
+        self.sheet = self.workbook.active
 
-    def from_template(self):
-        pass
+    def from_template(self, filename=None, filename_saveas=None):
+        assert (filename), "No filename provided to open as a template"
+        assert (filename_saveas), "No filename provided to save as new spreadsheet"  # noqa: E501
+        self.filename = filename_saveas
+        self.workbook =load_workbook(filename)
+        self.sheet = self.workbook.active
 
     def close(self):
-        pass
+        self.workbook = None
+        self.worksheet = None
+        self.filename = None
+        self.filename_saveas = None
 
     def saveas(self, filename):
+        self.filename = filename
+        self.filename_saveas = None
         self.workbook.save(filename)
 
     def save(self):
